c74ac4a02b0ac244adb68dc2d93143d595152e41
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-08-11 11:22:37 -0600

    feat: add input field/ button/ rendering of saved inputs and styling

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-<html>
-  <head>
-    <link rel="stylesheet" href="index.css">
-  </head>
-  <body>
-    <!-- Create an input element with type="text" and id="input-el" -->
-    <!-- Create a SAVE INPUT button with id="input-btn" -->
-    <script src="index.js"></script>
-  </body>
-</html>
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+
+inputBtn.addEventListener("click", function () {
+  myLeads.push(inputEl.value)
+  inputEl.value = ""
+  renderLeads()
+})
+
+function renderLeads() {
+  let listItems = ""
+  for (let i = 0; i < myLeads.length; i++) {
+    // listItems += "<li><a target='' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
+    listItems += `
+                  <li>
+                      <a target='_blank' href="${myLeads[i]}">
+                        ${myLeads[i]}
+                      </a>
+                  </li>`
+  }
+  ulEl.innerHTML = listItems
+}
