50bd918a030491f8f41a70c88f1f507f546a8e23
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-08-13 16:15:01 -0600

    add tab api

--- index.js
+++ index.js
@@ -1,34 +1,54 @@
 let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
+const deleteBtn = document.getElementById("delete-btn")
+const tabBtn = document.getElementById("tab-btn")
+
 const ulEl = document.getElementById("ul-el")
 
-let leadsFromLocalStorage = JSON.parse( localStorage.getItem('myLeads') )
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem('myLeads') )
 
 if (leadsFromLocalStorage) {
   myLeads = leadsFromLocalStorage
-  renderLeads()
+  render(myLeads)
 } 
 
-inputBtn.addEventListener("click", function () {
-  myLeads.push(inputEl.value)
-  localStorage.setItem("myLeads", JSON.stringify(myLeads))
-  console.log(localStorage.getItem("myLeads"))
+tabBtn.addEventListener("click", function() {
+  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
 
-  inputEl.value = ""
-  renderLeads()
+    myLeads.push(tabs[0].url)
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads)
+  })
 })
 
-function renderLeads() {
+function render(leads) {
   let listItems = ""
-  for (let i = 0; i < myLeads.length; i++) {
-    // listItems += "<li><a target='' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
+  for (let i = 0; i < leads.length; i++) {
     listItems += `
                   <li>
-                      <a target='_blank' href="${myLeads[i]}">
-                        ${myLeads[i]}
+                      <a target='_blank' href="${leads[i]}">
+                        ${leads[i]}
                       </a>
                   </li>`
   }
   ulEl.innerHTML = listItems
 }
+
+inputBtn.addEventListener("click", function () {
+  myLeads.push(inputEl.value)
+  localStorage.setItem("myLeads", JSON.stringify(myLeads))
+  inputEl.value = ""
+  render(myLeads)
+})
+
+deleteBtn.addEventListener("dblclick", function() {
+  localStorage.clear()
+  myLeads = []
+  render(myLeads)
+
+})
+
+
+
+
